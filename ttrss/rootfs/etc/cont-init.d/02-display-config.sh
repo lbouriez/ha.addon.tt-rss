30a46d12274b2e3268259e4b238ff43037acacb4
Laurent Bouriez <laurent@bouriez.org>
2021-04-05 21:19:00 -0400

    0.10.1

--- ttrss/rootfs/etc/cont-init.d/02-display-config.sh
+++ ttrss/rootfs/etc/cont-init.d/02-display-config.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/with-contenv bashio
-if [ ${log_level} = "debug" ]; then
-    bashio::log.blue \
-        '-----------------------------------------------------------'
-    bashio::log.blue " Here is the configuration on boot"
-    bashio::log.blue " TTRSS_DB_TYPE => ${TTRSS_DB_TYPE}"
-    bashio::log.blue " TTRSS_DB_HOST => ${TTRSS_DB_HOST}"
-    bashio::log.blue " TTRSS_DB_USER => ${TTRSS_DB_USER}"
-    bashio::log.blue " TTRSS_DB_NAME => ${TTRSS_DB_NAME}"
-    bashio::log.blue " TTRSS_DB_PASS => *******"
-    bashio::log.blue " TTRSS_DB_PORT => ${TTRSS_DB_PORT}"
-    bashio::log.blue " TTRSS_SELF_URL_PATH => ${TTRSS_SELF_URL_PATH}"
-    bashio::log.blue \
-        '-----------------------------------------------------------'
+if bashio::config.exists log_level; then
+    if [ "${log_level:=off}" = "debug" ]; then
+        bashio::log.blue \
+            '-----------------------------------------------------------'
+        bashio::log.blue " Here is the configuration on boot"
+        bashio::log.blue " TTRSS_DB_TYPE => ${TTRSS_DB_TYPE:?}"
+        bashio::log.blue " TTRSS_DB_HOST => ${TTRSS_DB_HOST:?}"
+        bashio::log.blue " TTRSS_DB_USER => ${TTRSS_DB_USER:?}"
+        bashio::log.blue " TTRSS_DB_NAME => ${TTRSS_DB_NAME:?}"
+        bashio::log.blue " TTRSS_DB_PASS => *******"
+        bashio::log.blue " TTRSS_DB_PORT => ${TTRSS_DB_PORT:?}"
+        bashio::log.blue " TTRSS_SELF_URL_PATH => ${TTRSS_SELF_URL_PATH:?}"
+        bashio::log.blue \
+            '-----------------------------------------------------------'
+    fi
 fi
